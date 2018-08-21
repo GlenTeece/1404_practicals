8849a9a2b12557fddd16c915282199a47ab17f80
Glen Teece <teece.glen@gmail.com>
2018-08-21 15:55:27 +1000

    Move prac 3 contents to prac_03 folder

---
+++ Prac_03/shop_calculator_refactored.py
@@ -0,0 +1,12 @@
+number_of_items = int(input("Number of Items: "))
+while number_of_items < 0:
+    print("Invalid number of items!")
+    number_of_items = int(input("Number of Items: "))
+total_cost = 0
+for i in range(number_of_items):
+    item_price = float(input("Price of Item: "))
+    total_cost += item_price
+print("Total price for "+str(number_of_items)+" item/s is "+str(total_cost))
+
+def item_combiner():
+    
