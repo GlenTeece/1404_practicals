c0629c23cfd6de78f1d277a1290db7eaf3169e8e
Glen Teece <teece.glen@gmail.com>
2018-08-28 14:24:03 +1000

    update Prac_04 work with list comprehension

--- Prac_04/five_numbers.py
+++ Prac_04/five_numbers.py
@@ -19,3 +19,14 @@ print("The last number is {}".format(numbers[-1]))
 print("The smallest number is {}".format(smallest))
 print("The largest number is {}".format(largest))
 print("The average number is {}".format(average))
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+input_name = input("Please enter your username")
+access_granted = False
+for name in usernames:
+    if name == input_name:
+        access_granted = True
+if access_granted:
+    print("Access Granted")
+else:
+    print("Access Denied")
--- Prac_04/lists_warmup.py
+++ Prac_04/lists_warmup.py
@@ -1,11 +1,13 @@
 numbers = [3, 1, 4, 1, 5, 9, 2]
 # What values do the following expressions have?
-print(numbers[0])  # 3
-print(numbers[-1]) # 2
-print(numbers[3]) # 1
-print(numbers[:-1]) #
-print(numbers[3:4]) #
-print(5 in numbers) #
-print(7 in numbers) #
-print("3" in numbers)#
-print(numbers + [6, 5, 3]) #
+# numbers[0] = 3
+# numbers[-1] = 2
+# numbers[3] = 1
+# numbers[:-1] =[3, 1, 4, 1, 5, 9, 2]
+# numbers[3:4] =1
+# 5 in numbers =
+# 7 in numbers =
+# "3" in numbers =
+# numbers + [6, 5, 3] =
+
+
